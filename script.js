77d19df77805d16b20cd2411fb7960a96c9b8a3b
Ben Iverson <2513iverson989@gmail.com>
2022-08-31 19:39:35 -0500

    1st commit was from memory, 2nd is the solution

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// I wrote this from memory, after reading about fizzbuzz months ago while doing research. it was wrong, but it worked.
+/*
 function fizzBuzz(iterations) {
   let lastWord = "buzz";
   let fizz = "fizz";
@@ -9,6 +11,22 @@ function fizzBuzz(iterations) {
     } else {
       console.log(buzz);
       lastWord = buzz;
+    }
+  }
+}
+*/
+
+//I wrote this after reading page 37 reminded me what the FizzBuzz challenge actually is
+function fizzBuzz(iterations) {
+  for (let i = 0; i < iterations; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
     }
   }
 }
